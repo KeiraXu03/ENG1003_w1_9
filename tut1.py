f827d3eddfe99cc5d0bce277b74b19282f51088f
KeiraXu <62126501+KeiraXu03@users.noreply.github.com>
2021-09-17 21:26:07 +0800

    Update tut1.py

--- tut1.py
+++ tut1.py
@@ -1,12 +1,32 @@
 print("This is ENG1003 Week1 Tutorial Programming Task")
-a=input("enter the function  number to be executed:")
-def put():
+g=input("enter the function  number to be executed:")
+def put1():
     print("jason ")
     print("XU ZHUONING ")
     print("HUYUXIN ")
     print("MAT")
     print("Asaad ARSHAD")
-if int(a) == 1:
-  put()
-else :
-    print("!")
+def put2():
+    a=5
+    b=7
+    c=9
+    Sum=a+b+c
+    print("a = ",a," b = ",b," c = ",c ,"  Sum = ",Sum)
+def put3():
+    a=5
+    print("a = ",a," , ","Square of a: ",a*a)
+def put4():
+    a=5
+    b=4
+    if a>b:
+        print(a,"is larger than ",b)
+    else:
+        print(b,"is larger than ",a)
+if int(g) == 1:
+  put1()
+elif int(g) == 2:
+  put2()
+elif int(g) == 3:
+  put3()
+elif int(g)==4:
+  put4()
