614d19da84b50c35b44ca3b16a57a22ec645e7a5
KeiraXu <62126501+KeiraXu03@users.noreply.github.com>
2021-09-21 16:22:16 +0800

    Update tut1.py

--- tut1.py
+++ tut1.py
@@ -1,32 +1,39 @@
 print("This is ENG1003 Week1 Tutorial Programming Task")
 g=input("enter the function  number to be executed:")
-def put1():
-    print("jason ")
-    print("XU ZHUONING ")
-    print("HUYUXIN ")
-    print("MAT")
-    print("Asaad ARSHAD")
-def put2():
-    a=5
-    b=7
-    c=9
-    Sum=a+b+c
-    print("a = ",a," b = ",b," c = ",c ,"  Sum = ",Sum)
-def put3():
-    a=5
-    print("a = ",a," , ","Square of a: ",a*a)
-def put4():
-    a=5
-    b=4
-    if a>b:
-        print(a,"is larger than ",b)
-    else:
-        print(b,"is larger than ",a)
+def put1(height):
+    height2=0
+    while(height2<=height):
+        print("XXX")
+        height2=height2+1
+def put2(first,second):
+    print("The sum is:",first+second)
+def put3(length):
+    length0=0
+    length2=length
+    while(length0<length):
+        print(length2)
+        length0=length0+1
+        length2=length2-1
+def put4(size0):
+    size1=0
+    size2=0
+    while(size1<size0):
+        while(size2<size0-1):
+            print("[]",end="")
+            size2+=1  
+        print("[]")    
+        size1+=1
+        size2=0
 if int(g) == 1:
-  put1()
+  height1=int(input("height:"))
+  put1(height1)
 elif int(g) == 2:
-  put2()
+  first1=int(input("First number:"))
+  second1=int(input("Second number:"))
+  put2(first1,second1)
 elif int(g) == 3:
-  put3()
+  length1=int(input("List length:"))
+  put3(length1)
 elif int(g)==4:
-  put4()
+  size=int(input("Square size:"))
+  put4(size)
