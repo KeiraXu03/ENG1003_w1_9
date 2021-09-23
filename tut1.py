28cc4df6b37b6bc7a4721b16d69c9e0e9f04b51b
KeiraXu <62126501+KeiraXu03@users.noreply.github.com>
2021-09-23 22:04:25 +0800

    Update tut1.py

--- tut1.py
+++ tut1.py
@@ -1,9 +1,7 @@
-print("This is ENG1003 Week1 Tutorial Programming Task")
-g=input("enter the function  number to be executed:")
-import random
+import random  
 def put1(height):
     height2=0
-    while(height2<=height):
+    while(height2<height):
         print("XXX")
         height2=height2+1
 def put2(first,second):
@@ -26,7 +24,6 @@ def put4(size0):
         size1+=1
         size2=0
 def put5():
-    import random
     a2=int(random.randint(1,200))
     b=int(random.randint(1,a2))
     print("Number Guessing Game")
@@ -47,18 +44,26 @@ def put5():
           down=c
           print("\n")
       elif(c==b):
-          print("BINGO! , You took",d-1,"attempts to reach the answer")  
-if int(g) == 1:
-  height1=int(input("height:"))
-  put1(height1)
-elif int(g) == 2:
-  first1=int(input("First number:"))
-  second1=int(input("Second number:"))
-  put2(first1,second1)
-elif int(g) == 3:
-  length1=int(input("List length:"))
-  put3(length1)
-elif int(g)==4:
-  size=int(input("Square size:"))
-elif int(g)==5:
-    put5()
+          print("BINGO! , You took",d-1,"attempts to reach the answer")
+print("This is ENG1003 Week1 Tutorial Programming Task")
+g=int(1)
+while(g==1 or g==2 or g==3 or g==4 or g==5):
+     g=int(input("enter the function  number to be executed:"))
+     import random  
+     if int(g) == 1:
+        height1=int(input("height:"))
+        put1(height1)
+     elif int(g) == 2:
+        first1=int(input("First number:"))
+        second1=int(input("Second number:"))
+        put2(first1,second1)
+     elif int(g) == 3:
+        length1=int(input("List length:"))
+        put3(length1)
+     elif int(g)==4:
+        size=int(input("Square size:"))
+        put4(size)
+     elif int(g)==5:
+        put5()
+     elif (g!=1 and g!=2 and g!=3 and g!=4 and g!=5):
+        print("wrong  number!")
