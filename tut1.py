498a96f32f94c05b101d90db599eca76f55f5705
KeiraXu <62126501+KeiraXu03@users.noreply.github.com>
2021-09-23 21:32:17 +0800

    Update tut1.py

--- tut1.py
+++ tut1.py
@@ -1,5 +1,6 @@
 print("This is ENG1003 Week1 Tutorial Programming Task")
 g=input("enter the function  number to be executed:")
+import random
 def put1(height):
     height2=0
     while(height2<=height):
@@ -24,6 +25,29 @@ def put4(size0):
         print("[]")    
         size1+=1
         size2=0
+def put5():
+    import random
+    a2=int(random.randint(1,200))
+    b=int(random.randint(1,a2))
+    print("Number Guessing Game")
+    print("The range for this round is 1 -",a2)
+    c=int(1)
+    d=int(1)
+    up=a2
+    down=int(1)
+    while(c!=b):
+      d+=1
+      c=int(input("Make a Guess:"))
+      if(c>b):
+          print("The range is now ",down,"-",c,end="!")
+          print("\n")
+          up=c
+      elif(c<b):
+          print("The range is now",c,"-",up,end="!")
+          down=c
+          print("\n")
+      elif(c==b):
+          print("BINGO! , You took",d-1,"attempts to reach the answer")  
 if int(g) == 1:
   height1=int(input("height:"))
   put1(height1)
@@ -36,4 +60,5 @@ elif int(g) == 3:
   put3(length1)
 elif int(g)==4:
   size=int(input("Square size:"))
-  put4(size)
+elif int(g)==5:
+    put5()
